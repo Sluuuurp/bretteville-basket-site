f297cdfac9e81e60c441c8b6a25a872a83e3cb83
ALEXANDRE Florian <florian.alexandre17@gmail.com>
2026-01-29 13:05:47 +0100

    reorganisation des fichiers dans le controller et mise a jour des routes, ajout de la gestion du deroulement du calendrier pour la page equipe en js, commit pre creation du crud evenement

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,46 @@
 console.log('Hello World')
+
+//gestion du depliment du calendrier de la liste des equipes
+document.querySelectorAll('.team-item').forEach((item) => {
+  item.addEventListener('click', async () => {
+    const calendarDiv = item.querySelector('.calendar')
+    const matchesDiv = calendarDiv.querySelector('.matches')
+    const loadingDiv = calendarDiv.querySelector('.loading')
+
+    calendarDiv.classList.toggle('hidden')
+
+    if (calendarDiv.dataset.loaded) return
+
+    loadingDiv.style.display = 'block'
+
+    try {
+      const codeteam = encodeURIComponent(item.dataset.link.split('/').pop())
+      const res = await fetch(`/calendrier?codeteam=${codeteam}`)
+      const teamCal = await res.json() // ceci est { calendar: [...] }
+      const matches = teamCal.calendar // tableau de matchs
+
+      matchesDiv.innerHTML = teamCal.calendar
+        .map(
+          (match) => `
+    <div class="match border rounded p-4 flex gap-4 items-center bg-white shadow">
+      <div class="flex flex-col">
+        <p class="font-bold">Numéro: ${match.numero}</p>
+        <p>Journée: ${match.journee}</p>
+        <p>Date: ${match.date}</p>
+        <p>Équipe: ${match.equipe}</p>
+        <p>Score: ${match.scoreBBC} - ${match.scoreExt}</p>
+      </div>
+    </div>
+  `
+        )
+        .join('')
+
+      calendarDiv.dataset.loaded = 'true'
+    } catch (err) {
+      matchesDiv.textContent = 'Erreur lors du chargement'
+      console.error(err)
+    } finally {
+      loadingDiv.style.display = 'none'
+    }
+  })
+})
--- start/routes.ts
+++ start/routes.ts
@@ -11,8 +11,8 @@ import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 
-router.get('/equipes', '#controllers/teams_list_controller.show')
+router.get('/equipes', '#controllers/team_data/teams_list_controller.show')
 
-router.get('/calendrier', '#controllers/team_calendar_controller.show')
+router.get('/calendrier', '#controllers/team_data/team_calendar_controller.show')
 
-router.get('/classement', '#controllers/team_ranking_controller.show')
+router.get('/classement', '#controllers/team_data/team_ranking_controller.show')
