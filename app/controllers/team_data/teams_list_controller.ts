d5814d145540fb20b11ee10ae902f12d2ab82047
ALEXANDRE Florian <florian.alexandre17@gmail.com>
2026-02-23 11:38:46 +0100

    fin du layout, debut de la vue teamlist avec la gestion d'affichage entre masc et fem

--- app/controllers/team_data/teams_list_controller.ts
+++ app/controllers/team_data/teams_list_controller.ts
@@ -2,14 +2,23 @@ import type { HttpContext } from '@adonisjs/core/http'
 import FfbbScrapService from '#services/ffbb_scrapper_services'
 
 export default class TeamsListController {
-  public async show({ view }: HttpContext) {
+  public async show({ request, view }: HttpContext) {
     const scraper = new FfbbScrapService()
 
     const teams = await scraper.getTeam(
       'https://competitions.ffbb.com/ligues/nor/comites/0014/clubs/nor0014052'
     )
 
+    const gender = request.input('gender') || 'M' // mon M ou F
+    const teamsFiltered = teams.filter((team) => team.tabData[0].includes(gender))
     // Ici on rend la page Edge et on passe les données
-    return view.render('pages/teams/index', { teams })
+
+    if (request.input('gender')) {
+      // Si c'est un fetch AJAX, on retourne **la partielle uniquement**
+      return view.render('components/teams-list', { teams: teamsFiltered })
+    }
+
+    // Sinon on retourne la page complète
+    return view.render('pages/teams/index', { teams: teamsFiltered })
   }
 }
