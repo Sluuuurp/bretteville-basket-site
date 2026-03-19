463d798719fafcf1ce91d0cb99dec82199e188c6
ALEXANDRE Florian <florian.alexandre17@gmail.com>
2026-03-19 13:57:51 +0100

    correction du bug de suppresion dans le panier

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -16,29 +16,32 @@ export function initCart() {
       cartContainer.insertAdjacentHTML(
         'beforeend',
         `
-        <div>
-          ${item.name} - ${item.size} × ${item.quantity}
+        <div class='border-y border-gray-400 py-1'>
+          <span class='font-bold'>${item.name}</span>&nbsp; - &nbsp; Taille:&nbsp; <span class='font-bold'>${item.size}</span>&nbsp;&nbsp; | &nbsp;&nbsp; Qte:&nbsp; <span class='font-bold'>${item.quantity}</span> &nbsp;&nbsp; | &nbsp;&nbsp; ${item.price * item.quantity}€  &nbsp; &nbsp; &nbsp;
 
           <input type="hidden" name="items[${index}][articleId]" value="${item.articleId}">
           <input type="hidden" name="items[${index}][size]" value="${item.size}">
           <input type="hidden" name="items[${index}][quantity]" value="${item.quantity}">
 
-          <button type="button" class="remove-item" data-index="${index}">✕</button>
+          <button type="button" class="remove-item bg-red-500 text-sm px-1 rounded text-white" data-id="${item.id}">✕ </button>
         </div>
       `
       )
 
       document.querySelectorAll('.remove-item').forEach((btn) => {
         btn.addEventListener('click', () => {
-          removeItem(Number(btn.dataset.index))
+          removeItem(Number(btn.dataset.id))
         })
       })
     })
     document.getElementById('cart-total').textContent = `Total : ${totalCart} €`
   }
 
-  function removeItem(index) {
-    cart.splice(index, 1)
+  function removeItem(id) {
+    const index = cart.findIndex((item) => item.id === id)
+    if (index !== -1) {
+      cart.splice(index, 1)
+    }
     renderCart()
   }
 
@@ -61,6 +64,7 @@ export function initCart() {
         existingItem.quantity += quantity
       } else {
         cart.push({
+          id: Date.now() + Math.random(),
           articleId,
           name,
           size,
