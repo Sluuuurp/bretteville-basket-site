0f8acf0c47aa142cc2b3638a2c8afa6150d583da
ALEXANDRE Florian <florian.alexandre17@gmail.com>
2026-04-22 21:25:01 +0200

    correction de la version mobile du calendar, de l' acceuil et du rankings,

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -36,22 +36,22 @@ export function initCalendar() {
                 : 'md:bg-[rgba(0,101,160,0.2)]'
               : 'md:bg-white'
           return `
-      <div class="match mt-5 md:mt-10 p-4 flex items-center ${bgclass} font-manrope max-w-375">
+      <div class="match mt-5 mb-5  flex items-center ${bgclass} font-manrope max-w-375">
         <div class="grid grid-cols-6 font-bold items-center relative">
          
           <p class=' w-50 col-start-1 text-sm'><span class='uppercase mr-5 md:mr-0.5 xl:mr-5'>${match.journee}</span> | <span class='opacity-65 ml-5 md:ml-0.5 xl:ml-5'>${match.date}</span></p>
-          <p class='uppercase text-sm  md:text-center opacity-50 col-start-6 md:col-start-2 xl:col-start-2 '>${match.lieu}</p>
+          <p class='uppercase text-sm  md:text-center opacity-50 col-start-5 md:col-start-2 xl:col-start-2 '>${match.lieu}</p>
           <div class='h-5 w-full ${colorGender} absolute top-0 md:hidden'></div>
           <div class='flex h-10 md:h-15 text-center col-start-1 col-span-6 md: md:col-start-3 mt-2'>
-            <p class='uppercase w-30 text-xs m-auto'>Bretteville Basket Cingal</p>
+            <p class='uppercase w-30 text-[0.65rem] sm:text-xs m-auto'>Bretteville Basket Cingal</p>
             <img class='' src='/img/logo-bbc.png'>
-            <div class='flex items-center h-15 md:h-20 gap-3 md:gap-6 font-teko text-4xl'>
+            <div class='flex items-center h-15 md:h-20 gap-3 md:gap-6 font-teko text-2xl md:text-4xl'>
               <span> ${match.scoreBBC || '0'}</span>
-              <img class='' src='/img/versus.png'>
+              <img class='w-5 md:w-10' src='/img/versus.png'>
               <span>${match.scoreExt}</span>
             </div>
             <img class='ml-10' src=${match.img && !match.img.includes('defaultLogo') ? match.img : '/img/logo-ffbb.jpg'} />
-            <p class='uppercase w-30 text-xs  m-auto'>${match.equipe}</p>
+            <p class='uppercase w-30 text-[0.65rem] sm:text-xs  m-auto'>${match.equipe}</p>
           </div>
         </div>
       </div>
